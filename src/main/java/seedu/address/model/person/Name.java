46d37d40a51be097ecbf61b5b5e8c5e3f2fa87a2
zheng-jj <zhengjiongjie12345@gmail.com>
2025-11-03 21:38:42 +0800

    update name constraints

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,13 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS = "Names should only contain alphanumeric characters, spaces, "
-            + "apostrophes (') and '@', and should not be blank";
+            + "apostrophes ('), '/' and '@', and should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}'@][\\p{Alnum}'@ ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}'@/][\\p{Alnum}'@ /]*";
 
     public final String fullName;
 
