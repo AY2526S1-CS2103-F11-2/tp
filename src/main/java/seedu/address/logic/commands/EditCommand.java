770155077d678c7c5f66ff37c9bd4c5e47f987af
TzHsien <limtzehsien@gmail.com>
2025-11-03 21:45:55 +0800

    Fix edit error message to be more readable and concise

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -38,10 +38,8 @@ public class EditCommand extends Command {
 
     public static final String COMMAND_WORD = "edit";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
-            + "by the index number used in the displayed person list. "
-            + "Existing values will be overwritten by the input values.\n"
-            + "Parameters: INDEX (must be a positive integer) "
+    public static final String MESSAGE_USAGE = "Please follow the format as follows: \n"
+            + COMMAND_WORD + " INDEX (must be a positive integer) \n"
             + "[" + PREFIX_NAME + "NAME] "
             + "[" + PREFIX_TELEHANDLE + "TELEHANDLE] "
             + "[" + PREFIX_INSTRUMENT + "INSTRUMENT] "
